05a44b76ae234fa8d52f1db03adf61d0b3ce400d
shoulinwei <weishoulin@astrolab.cn>
2021-08-10 10:17:15 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,38 +1,5 @@
 # coding: utf-8
-"""Setup config file to package the configuration database."""
-from os import walk, listdir
-from setuptools import find_packages
-from os.path import join
+import os
 from setuptools import setup
 
-
-def package_files(directory):
-    """Get list of data files to add to the package."""
-    paths = []
-    for (path, _, file_names) in walk(directory):
-        for filename in file_names:
-            paths.append(join('..', path, filename))
-    return paths
-
-
-with open('README.md', 'r') as file:
-    LONG_DESCRIPTION = file.read()
-
-setup(name='csst_dfs_proto',
-      version='0.0.1',
-      author='CSST DFS team.',
-      description='CSST DFS Base Proto library.',
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      url='',
-      packages=find_packages(exclude=('test', 'test.*', 'fabfile')),
-      install_requires=[
-          'grpcio==1.28.1'
-      ],
-      zip_safe=False,
-      classifiers=[
-          "Programming Language :: Python :: 3 :: Only",
-          "Development Status :: 1 - Planning",
-          "License :: OSI Approved :: BSD License"
-      ]
-)
+setup(use_scm_version={'write_to': os.path.join('csst_dfs_proto', '_version.py')})
