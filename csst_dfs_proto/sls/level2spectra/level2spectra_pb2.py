d3f0b29f230dca3903aeabed932d54d1f1b3ea4d
shoulinwei <weishoulin@astrolab.cn>
2023-07-05 15:42:31 +0800

    update sls 2

--- csst_dfs_proto/sls/level2spectra/level2spectra_pb2.py
+++ csst_dfs_proto/sls/level2spectra/level2spectra_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 from ...common import error_pb2 as common_dot_error__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n%sls/level2spectra/level2spectra.proto\x12\x15\x64\x66s.sls.level2spectra\x1a\x12\x63ommon/error.proto\"\xf5\x01\n\x13Level2spectraRecord\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x11\n\tlevel0_id\x18\x02 \x01(\t\x12\x11\n\tlevel1_id\x18\x03 \x01(\x03\x12\x11\n\tfile_type\x18\x04 \x01(\t\x12\x10\n\x08\x66ilename\x18\x05 \x01(\t\x12\x11\n\tfile_path\x18\x06 \x01(\t\x12\x12\n\nqc2_status\x18\x07 \x01(\x05\x12\x10\n\x08qc2_time\x18\x08 \x01(\t\x12\x12\n\nprc_status\x18\t \x01(\x05\x12\x10\n\x08prc_time\x18\n \x01(\t\x12\x13\n\x0b\x63reate_time\x18\x0b \x01(\t\x12\x13\n\x0bpipeline_id\x18\x0c \x01(\t\"\xcd\x02\n\x14\x46indLevel2spectraReq\x12\x11\n\tlevel0_id\x18\x01 \x01(\t\x12\x11\n\tlevel1_id\x18\x02 \x01(\x03\x12\x19\n\x11\x63reate_time_start\x18\x03 \x01(\t\x12\x17\n\x0f\x63reate_time_end\x18\x04 \x01(\t\x12\x12\n\nqc2_status\x18\x05 \x01(\x05\x12\x12\n\nprc_status\x18\x06 \x01(\x05\x12\x10\n\x08\x66ilename\x18\x07 \x01(\t\x12\r\n\x05limit\x18\x08 \x01(\x05\x12Z\n\x10other_conditions\x18\t \x03(\x0b\x32@.dfs.sls.level2spectra.FindLevel2spectraReq.OtherConditionsEntry\x1a\x36\n\x14OtherConditionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\x97\x01\n\x15\x46indLevel2spectraResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x12\n\ntotalCount\x18\x02 \x01(\x05\x12\x1c\n\x05\x65rror\x18\x03 \x01(\x0b\x32\r.common.Error\x12;\n\x07records\x18\x04 \x03(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"!\n\x13GetLevel2spectraReq\x12\n\n\x02id\x18\x01 \x01(\x03\"R\n\x14GetLevel2spectraResp\x12:\n\x06record\x18\x01 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"a\n\x15WriteLevel2spectraReq\x12:\n\x06record\x18\x01 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\"\x83\x01\n\x16WriteLevel2spectraResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error\x12:\n\x06record\x18\x03 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"0\n\x12UpdateQc2StatusReq\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0e\n\x06status\x18\x02 \x01(\x05\"D\n\x13UpdateQc2StatusResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error\"1\n\x13UpdateProcStatusReq\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0e\n\x06status\x18\x02 \x01(\x05\"E\n\x14UpdateProcStatusResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error2\x9e\x04\n\x10Level2spectraSrv\x12\x63\n\x04\x46ind\x12+.dfs.sls.level2spectra.FindLevel2spectraReq\x1a,.dfs.sls.level2spectra.FindLevel2spectraResp\"\x00\x12`\n\x03Get\x12*.dfs.sls.level2spectra.GetLevel2spectraReq\x1a+.dfs.sls.level2spectra.GetLevel2spectraResp\"\x00\x12h\n\x05Write\x12,.dfs.sls.level2spectra.WriteLevel2spectraReq\x1a-.dfs.sls.level2spectra.WriteLevel2spectraResp\"\x00(\x01\x12j\n\x0fUpdateQc2Status\x12).dfs.sls.level2spectra.UpdateQc2StatusReq\x1a*.dfs.sls.level2spectra.UpdateQc2StatusResp\"\x00\x12m\n\x10UpdateProcStatus\x12*.dfs.sls.level2spectra.UpdateProcStatusReq\x1a+.dfs.sls.level2spectra.UpdateProcStatusResp\"\x00\x42(Z&cnlab.net/csst/proto/sls/level2spectrab\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n%sls/level2spectra/level2spectra.proto\x12\x15\x64\x66s.sls.level2spectra\x1a\x12\x63ommon/error.proto\"\xf5\x01\n\x13Level2spectraRecord\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x11\n\tlevel0_id\x18\x02 \x01(\t\x12\x11\n\tlevel1_id\x18\x03 \x01(\x03\x12\x11\n\tfile_type\x18\x04 \x01(\t\x12\x10\n\x08\x66ilename\x18\x05 \x01(\t\x12\x11\n\tfile_path\x18\x06 \x01(\t\x12\x12\n\nqc2_status\x18\x07 \x01(\x05\x12\x10\n\x08qc2_time\x18\x08 \x01(\t\x12\x12\n\nprc_status\x18\t \x01(\x05\x12\x10\n\x08prc_time\x18\n \x01(\t\x12\x13\n\x0b\x63reate_time\x18\x0b \x01(\t\x12\x13\n\x0bpipeline_id\x18\x0c \x01(\t\"\xe0\x02\n\x14\x46indLevel2spectraReq\x12\x11\n\tlevel0_id\x18\x01 \x01(\t\x12\x11\n\tlevel1_id\x18\x02 \x01(\x03\x12\x11\n\tfile_type\x18\x03 \x01(\x03\x12\x19\n\x11\x63reate_time_start\x18\x04 \x01(\t\x12\x17\n\x0f\x63reate_time_end\x18\x05 \x01(\t\x12\x12\n\nqc2_status\x18\x06 \x01(\x05\x12\x12\n\nprc_status\x18\x07 \x01(\x05\x12\x10\n\x08\x66ilename\x18\x08 \x01(\t\x12\r\n\x05limit\x18\t \x01(\x05\x12Z\n\x10other_conditions\x18\n \x03(\x0b\x32@.dfs.sls.level2spectra.FindLevel2spectraReq.OtherConditionsEntry\x1a\x36\n\x14OtherConditionsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\x97\x01\n\x15\x46indLevel2spectraResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x12\n\ntotalCount\x18\x02 \x01(\x05\x12\x1c\n\x05\x65rror\x18\x03 \x01(\x0b\x32\r.common.Error\x12;\n\x07records\x18\x04 \x03(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"!\n\x13GetLevel2spectraReq\x12\n\n\x02id\x18\x01 \x01(\x03\"R\n\x14GetLevel2spectraResp\x12:\n\x06record\x18\x01 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"a\n\x15WriteLevel2spectraReq\x12:\n\x06record\x18\x01 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\"\x83\x01\n\x16WriteLevel2spectraResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error\x12:\n\x06record\x18\x03 \x01(\x0b\x32*.dfs.sls.level2spectra.Level2spectraRecord\"0\n\x12UpdateQc2StatusReq\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0e\n\x06status\x18\x02 \x01(\x05\"D\n\x13UpdateQc2StatusResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error\"1\n\x13UpdateProcStatusReq\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0e\n\x06status\x18\x02 \x01(\x05\"E\n\x14UpdateProcStatusResp\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x1c\n\x05\x65rror\x18\x02 \x01(\x0b\x32\r.common.Error2\x9e\x04\n\x10Level2spectraSrv\x12\x63\n\x04\x46ind\x12+.dfs.sls.level2spectra.FindLevel2spectraReq\x1a,.dfs.sls.level2spectra.FindLevel2spectraResp\"\x00\x12`\n\x03Get\x12*.dfs.sls.level2spectra.GetLevel2spectraReq\x1a+.dfs.sls.level2spectra.GetLevel2spectraResp\"\x00\x12h\n\x05Write\x12,.dfs.sls.level2spectra.WriteLevel2spectraReq\x1a-.dfs.sls.level2spectra.WriteLevel2spectraResp\"\x00(\x01\x12j\n\x0fUpdateQc2Status\x12).dfs.sls.level2spectra.UpdateQc2StatusReq\x1a*.dfs.sls.level2spectra.UpdateQc2StatusResp\"\x00\x12m\n\x10UpdateProcStatus\x12*.dfs.sls.level2spectra.UpdateProcStatusReq\x1a+.dfs.sls.level2spectra.UpdateProcStatusResp\"\x00\x42(Z&cnlab.net/csst/proto/sls/level2spectrab\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'sls.level2spectra.level2spectra_pb2', globals())
@@ -27,27 +27,27 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _LEVEL2SPECTRARECORD._serialized_start=85
   _LEVEL2SPECTRARECORD._serialized_end=330
   _FINDLEVEL2SPECTRAREQ._serialized_start=333
-  _FINDLEVEL2SPECTRAREQ._serialized_end=666
-  _FINDLEVEL2SPECTRAREQ_OTHERCONDITIONSENTRY._serialized_start=612
-  _FINDLEVEL2SPECTRAREQ_OTHERCONDITIONSENTRY._serialized_end=666
-  _FINDLEVEL2SPECTRARESP._serialized_start=669
-  _FINDLEVEL2SPECTRARESP._serialized_end=820
-  _GETLEVEL2SPECTRAREQ._serialized_start=822
-  _GETLEVEL2SPECTRAREQ._serialized_end=855
-  _GETLEVEL2SPECTRARESP._serialized_start=857
-  _GETLEVEL2SPECTRARESP._serialized_end=939
-  _WRITELEVEL2SPECTRAREQ._serialized_start=941
-  _WRITELEVEL2SPECTRAREQ._serialized_end=1038
-  _WRITELEVEL2SPECTRARESP._serialized_start=1041
-  _WRITELEVEL2SPECTRARESP._serialized_end=1172
-  _UPDATEQC2STATUSREQ._serialized_start=1174
-  _UPDATEQC2STATUSREQ._serialized_end=1222
-  _UPDATEQC2STATUSRESP._serialized_start=1224
-  _UPDATEQC2STATUSRESP._serialized_end=1292
-  _UPDATEPROCSTATUSREQ._serialized_start=1294
-  _UPDATEPROCSTATUSREQ._serialized_end=1343
-  _UPDATEPROCSTATUSRESP._serialized_start=1345
-  _UPDATEPROCSTATUSRESP._serialized_end=1414
-  _LEVEL2SPECTRASRV._serialized_start=1417
-  _LEVEL2SPECTRASRV._serialized_end=1959
+  _FINDLEVEL2SPECTRAREQ._serialized_end=685
+  _FINDLEVEL2SPECTRAREQ_OTHERCONDITIONSENTRY._serialized_start=631
+  _FINDLEVEL2SPECTRAREQ_OTHERCONDITIONSENTRY._serialized_end=685
+  _FINDLEVEL2SPECTRARESP._serialized_start=688
+  _FINDLEVEL2SPECTRARESP._serialized_end=839
+  _GETLEVEL2SPECTRAREQ._serialized_start=841
+  _GETLEVEL2SPECTRAREQ._serialized_end=874
+  _GETLEVEL2SPECTRARESP._serialized_start=876
+  _GETLEVEL2SPECTRARESP._serialized_end=958
+  _WRITELEVEL2SPECTRAREQ._serialized_start=960
+  _WRITELEVEL2SPECTRAREQ._serialized_end=1057
+  _WRITELEVEL2SPECTRARESP._serialized_start=1060
+  _WRITELEVEL2SPECTRARESP._serialized_end=1191
+  _UPDATEQC2STATUSREQ._serialized_start=1193
+  _UPDATEQC2STATUSREQ._serialized_end=1241
+  _UPDATEQC2STATUSRESP._serialized_start=1243
+  _UPDATEQC2STATUSRESP._serialized_end=1311
+  _UPDATEPROCSTATUSREQ._serialized_start=1313
+  _UPDATEPROCSTATUSREQ._serialized_end=1362
+  _UPDATEPROCSTATUSRESP._serialized_start=1364
+  _UPDATEPROCSTATUSRESP._serialized_end=1433
+  _LEVEL2SPECTRASRV._serialized_start=1436
+  _LEVEL2SPECTRASRV._serialized_end=1978
 # @@protoc_insertion_point(module_scope)
